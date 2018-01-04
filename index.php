41c7f1f68a6b70f74533eb5b226628ce962641e6
Reda Drehim <r.drehim@shiftebusiness.com>
2018-01-04 22:43:28 +0200

    updates on naming conventions

--- index.php
+++ index.php
@@ -9,7 +9,15 @@ $testTripRoot = new TripRoot($roots);
 $testTripSorter=$testTripRoot->getSortedRoots();
 ?>
 <h2>Your trip roots and nodes will be the below</h2>
+<div>
+		<div>Giving scenario you set the below</div>
+		<div>AirportRoot Barcelona, Gerona Airport</div>
+		<div>Flights Stockholm, New York JFK, 7B, SK22', 22 info</div>
+		<div>Trains statuions Madrid Barcelona 45B 78A</div>
+		<div>Then Flights info Gerona Airport, Stockholm, 3A, SK455, 45B', 344</div>
+</div>
 <ol>
+	
 <?php for ($i = 0; $i < count($testTripSorter); $i++) {?>
 	<li><?=$testTripSorter[$i]?></li>
  <?php } ?>
